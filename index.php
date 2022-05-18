a05330781173a9801bc7b727306e74a2cb115606
Kannen Joshi <kj265@sussex.ac.uk>
2022-05-18 02:27:46 +0100

    Make Look Nice w/ Logo Etc

--- index.php
+++ index.php
@@ -7,5 +7,6 @@ ini_set('display_errors',1);
 
 // The require or include statement takes all the text/code/markup that exists 
 // in the specified file and copies it into the file that uses the include (or require) statement.
+include_once 'config.php';
 require 'application/mvc.php';
 ?>
