708d756f03aedad872e9f7cfa327d4fb600ae29e
Kannen Joshi <kj265@sussex.ac.uk>
2022-05-19 05:27:31 +0100

    Header and Footer

---
+++ js/navigation.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+         
+    $('.nav-link').click(function() {
+        $('.nav-link').removeClass("active");
+        $(this).addClass("active");
+    });
+    
+
+    // GET RID OF TRANSFORM TAGS AROUND CAMERA AND MOVE IN TRANSFORM MOCE IN POS AND ROT INTO DEF OF CAMERA VIEW OR IT DIE COPY ACROSS TO MAKE CAM WORK WITH BUTTON JS OR IT NO LIKEY, Def camerafront or it kills have js model diff page god i hate this module
+ });
