eaa69909bae3766a67d07cdb50071b9acf6a4614
Kannen Joshi <kj265@sussex.ac.uk>
2022-05-19 12:19:54 +0100

    More 3D and Label Imgs

--- application/view/view_model_bottle.php
+++ application/view/view_model_bottle.php
@@ -11,15 +11,23 @@
     <!-- X3dom -->
     <link rel='stylesheet' type='text/css' href='css/x3dom.css'>
     <script src='js/x3dom.js'></script>
+    <script src='js/modelInteractions.js'></script>
 
         <h1>BOTTLE</h1>
         <br>
 
-        <x3d width='40vw' height='40vh' name='x3dBlock'> 
-            <scene>
-                <inline nameSpaceName="Bottle" url="assets/x3d/bottle.x3d"> </inline>
-            </scene>
-        </x3d> 
+        <div class="row">
+            <x3d id="model" class="col-sm" width='100%' height='100%' name='x3dBlock'> 
+                <scene>
+                    <inline nameSpaceName="model" mapDEFToID="true" onclick="animateModel();" url="assets/x3d/can.x3d"> </inline>
+                </scene>
+            </x3d>  
+
+            <div class="col-sm">
+                <div class="btn btn-secondary btn-responsive" onclick="wireframe();">Wire</div>
+                <div class="btn btn-success btn-responsive" onclick="headlight();">Headlight</div>
+            </div>
+        </div>
 
     <br>
     <!-- Footer -->
