993637f93fea21b83ca53c3fff1c8731528970b2
Kannen Joshi <kj265@sussex.ac.uk>
2022-05-19 11:33:00 +0100

    x3deeeeeeeeee

--- application/view/view_model_bottle.php
+++ application/view/view_model_bottle.php
@@ -1,19 +1,23 @@
 <!DOCTYPE HTML>
 
 <html lang="en">
-<body>
+<body>   
 
     <!-- Header -->
     <?php 
         require_once "header.html";
     ?>
+    
+    <!-- X3dom -->
+    <link rel='stylesheet' type='text/css' href='css/x3dom.css'>
+    <script src='js/x3dom.js'></script>
 
         <h1>BOTTLE</h1>
         <br>
 
-        <x3d width='60vw' height='40vh' name='x3dBlock'> 
+        <x3d width='40vw' height='40vh' name='x3dBlock'> 
             <scene>
-                <inline nameSpaceName="Bottle" url="../../assets/x3d/bottle.x3d"> </inline>
+                <inline nameSpaceName="Bottle" url="assets/x3d/bottle.x3d"> </inline>
             </scene>
         </x3d> 
 
